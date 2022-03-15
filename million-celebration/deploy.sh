5306d344fc0f244ccfef33209954253342afcf7b
dr666m1 <skndr666m1@gmail.com>
2022-03-16 00:42:16 +0900

    Update million_celebration/deploy.sh (update entrypoint)

--- million-celebration/deploy.sh
+++ million-celebration/deploy.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 gcloud workflows deploy million-celebration --source=workflow.yaml --service-account=daily-batch@${PROJECT}.iam.gserviceaccount.com --location=${REGION}
 
 cd ./functions
-gcloud functions deploy MillionCelebrationUpload \
+gcloud functions deploy Load \
   --runtime go116 --trigger-http --memory 2048MB --timeout 500s --region ${REGION} \
   --set-secrets YOUTUBE_KEY=YOUTUBE_KEY:latest,MILLION_CELEBRATION_SECRETS_JSON=MILLION_CELEBRATION_SECRETS_JSON:latest \
   --set-env-vars PROJECT=${PROJECT}
